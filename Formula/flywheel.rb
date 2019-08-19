58c3426c3b879f765bb5a4bd6969d48e77885690
Adam Lassek <adam.lassek@getflywheel.com>
2019-08-19 16:57:13 -0500

    stub flywheel formula

---
+++ Formula/flywheel.rb
@@ -0,0 +1,10 @@
+class Flywheel < Formula
+  desc "Meta-package for Flywheel Engineering"
+
+  version "1"
+
+  url "file:///dev/null"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+
+  depends_on "heroku/brew/heroku"
+end
