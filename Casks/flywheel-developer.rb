097a0211dba69946c41328e5ec064397744b38e8
Adam Lassek <adam.lassek@getflywheel.com>
2019-08-19 18:35:01 -0500

    switch to cask

---
+++ Casks/flywheel-developer.rb
@@ -0,0 +1,38 @@
+cask 'flywheel-developer' do
+  version :latest
+  sha256 :no_check
+
+  url "file:///dev/null"
+
+  name "Flywheel Development Environment"
+
+  depends_on formula: %w[
+               universal-ctags/universal-ctags/universal-ctags
+               git
+               openssl
+               reattach-to-user-namespace
+               the_silver_searcher
+               tmux
+               heroku/brew/heroku
+               hub
+               imagemagick@6
+               coreutils
+               direnv
+               libksba
+               libxml2
+               libyaml
+               pkg-config
+               puma-dev
+               mysql
+               redis
+               docker-compose
+               kubectl
+               packer
+               terraform
+             ],
+             cask: %w[
+               chromedriver
+               gpg-suite
+               docker
+             ]
+end
